b2b8aa6c5f9469444daa96b12dce1f905d905a5f
bojosteph <bojosteph@gmail.com>
2019-02-26 04:24:47 -0800

    Added controllers and views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,82 @@
 class EventsController < ApplicationController
   
-  get '/events' do
-    @events = Event.all
-    @user = current_user
-    erb :"/events/index.html"
+  get "/events" do
+    if logged_in?
+      @events = Event.all
+      erb :"/events/index.html"
+    else
+      redirect "login"
+    end
   end
 
+  get "/events/new" do
+    if logged_in?
+       erb :'/events/new.html'
+    else
+      redirect to "/login"
+    end
+  end
+
+  post "/events" do
+    params.each do |label, input|
+      if input.empty?
+        flash[:user_error] = "Please enter a value for #{label}"
+        redirect to "/signup"
+      end
+    end
+    user = current_user
+    @event = Event.create(:name => params[:name], :date => params[:date], :location => params[:location], :description => params[:description],:user_id => user.id)
+    
+    redirect to "/events"    
+  end
+
+  get "/events/:id" do
+    if !logged_in?
+      redirect to "/login"
+    end
+    @user = current_user
+    @event = Event.find(params[:id])
+    erb :"events/show_event.html"
+  end
+
+  get "/events/:id/edit" do
+    if !logged_in?
+      redirect to "/login"
+    end
+    @event = Event.find(params[:id])
+    if current_user.id != @event.user_id
+      flash[:invalid_user] = "You can only edit event that you created"
+      redirect to "/events"
+    end
+    erb :"events/edit_event.html"
+  end
+
+  patch "/events/:id" do
+    params.each do |label, input|
+      if input.empty?
+        flash[:user_error] = "Please enter a value for #{label}"
+        redirect to "/events/#{params[:id]}/edit"
+      end
+    end
+    event = Event.find(params[:id])
+    event.update(:name => params[:name], :date => params[:date], :location => params[:location], :description => params[:description])
+    event.save
+
+    redirect to "/events/#{event.id}"
+  end
+
+  delete "/events/:id/delete" do
+    if !logged_in?
+      redirect to "/login"
+    end
+    @event = Event.find(params[:id])
+    if current_user.id != @event.user_id
+      flash[:wrong_user] = "You can only delete your events"
+      redirect to "/events"
+    end
+    @event.delete
+    redirect to "/events"
+  end
+    
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,50 +1,65 @@
 class UsersController < ApplicationController
 
-  get '/signup' do
-
-    erb :'/users/signup.html'
-  end
-
-  post '/signup' do
+  get "/signup" do
     params.each do |label, input|
       if input.empty?
         flash[:user_error] = "Please enter a value for #{label}"
-        redirect to '/signup'
+        redirect to "/signup"
+      end
+    end
+    erb :"/users/signup.html"
+  end
+
+  post "/signup" do
+    params.each do |label, input|
+      if input.empty?
+        flash[:user_error] = "Please enter a value for #{label}"
+        redirect to "/signup"
       end
     end
 
     user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
     session[:user_id] = user.id
 
-    redirect to '/events'
+    redirect to "/events"
   end
 
-  get '/login' do
+  get "/login" do
     if logged_in?
-      redirect to '/events'
+      redirect to "/events"
     end
 
-    erb :'/users/login.html'
+    erb :"/users/login.html"
   end
   
-  post '/login' do
+  post "/login" do
     user = User.find_by(:username => params["username"])
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect to '/events'
+      redirect to "/events"
     else
       flash[:login_error] = "Incorrect login. Please try again."
-      redirect to '/login'
+      redirect to "/login"
     end
   end
 
-  get '/logout' do
+  get "/users/:id" do
+    if !logged_in?
+      redirect to "/login"
+    end
+    @user = current_user
+
+    erb :"users/show.html"
+  end
+
+
+  get "/logout" do
     if logged_in?
       session.clear
-      redirect to '/login'
+      redirect to "/login"
     else
-      redirect to '/'
+      redirect to "/"
     end
   end
   
