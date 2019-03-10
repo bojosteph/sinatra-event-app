f1046d9ea8ddad0e18f1de4a4a4eb71077179c71
bojosteph <bojosteph@gmail.com>
2019-03-10 14:00:31 -0700

    corrected issues with db-seed

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+User.create(username: "rob", email: "rob@test.com", password: "hello", full_name: "Roberto Samia")
+User.create(username: "rocky", email: "rocky@test.com", password: "adrian", full_name: "Rocky Balboa")
+User.create(username: "wonka", email: "wonka@test.com", password: "candy", full_name: "Willy Candy")
+
