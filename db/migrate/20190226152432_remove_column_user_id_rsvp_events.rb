3b50409bc9c964ebf764db11715880b74f0b7dc7
github username <github email address>
2019-03-18 22:47:19 -0400

    corrected issue with migration file

--- db/migrate/20190226152432_remove_column_user_id_rsvp_events.rb
+++ db/migrate/20190226152432_remove_column_user_id_rsvp_events.rb
@@ -1,5 +1,5 @@
 class RemoveColumnUserIdRsvpEvents < ActiveRecord::Migration
   def change
-    remove_column :rsvp_events, :user_id, :false
+    remove_column :rsvp_events, :user_id, :integer
   end
 end
